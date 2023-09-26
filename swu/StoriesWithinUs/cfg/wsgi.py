f969bf9e926da9815923f1326c4de6469bdb42b1
William Nguyen <nguyen_p3@denison.edu>
2023-09-25 23:47:16 -0400

    Relocate current work

--- swu/StoriesWithinUs/cfg/wsgi.py
+++ swu/StoriesWithinUs/cfg/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfg.settings')
 
+os.environ["DJANGO_SETTINGS_MODULE"] = "cfg.settings"
+
 application = get_wsgi_application()
